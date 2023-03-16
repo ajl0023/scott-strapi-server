9577a47a8da24b4fabd5d61bc0bfccba1748742a
ajl0023 <austinlee199723@yahoo.com>
2023-03-16 03:44:05 -0700

    edited db config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("NODE_ENV") === "development"
+          ? env("DATABASE_FILENAME", ".tmp/data.db")
+          : "/home/cleavr/0jkx6xkxdj7fwnnc7052.cleaver.live/storage/.tmp/data.db"
+      ),
     },
     useNullAsDefault: true,
   },
--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,12 @@
 module.exports = {
   ezforms: {
     config: {
+      enableFormName: true,
       captchaProvider: {
         name: "none",
       },
       notificationProviders: [],
     },
   },
+  
 };
