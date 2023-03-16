b24578c5197ddbdce21f325d1f209637330fa4e3
ajl0023 <austinlee199723@yahoo.com>
2023-03-16 04:49:15 -0700

    edited db config

--- config/database.js
+++ config/database.js
@@ -1,16 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "sqlite",
+    client: 'sqlite',
     connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        env("NODE_ENV") === "development"
-          ? env("DATABASE_FILENAME", ".tmp/data.db")
-          : "/home/cleavr/0jkx6xkxdj7fwnnc7052.cleaver.live/storage/.tmp/data.db"
-      ),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
   },
